25b2004c131676012a8fd78a955a95caff4000a6
Karenandreagomez <karenandreag98@hotmail.com>
2022-08-15 16:06:34 -0500

    Create ConvencionJava.java

---
+++ ConvencionJava.java
@@ -0,0 +1,18 @@
+public class ConvencionJava {
+    public static void main(String[] args) {
+        int celphone = 333337;
+        int celPhone = 555553;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Karen";
+        String _backgroundColor = "Green";
+        String  corrency$ = "MXN";
+        String back_ground_color = "Blue";
+
+        //CONSTANTES : VARIABLES QUE TIENEN UN VALOR FIJO (NO CAMBIA)
+        int POSITION = -5;
+        int MAX_WIDTH = 999;
+        int MIN_WIDTH = 1;
+    }
+}
