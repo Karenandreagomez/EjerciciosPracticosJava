c23364cc695ac626d287a3c5e0ac615328a3203d
Karenandreagomez <karenandreag98@hotmail.com>
2022-08-15 16:05:20 -0500

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarando una varible
+        int speed;
+        //Asignamos un valor a una variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary= 1000;
+        String employeeName = "karen gomez";
+        System.out.println(employeeName);
+    }
+}
