5acd956f262d31a8c4304ba4d1de096c5fd4eb97
Karenandreagomez <karenandreag98@hotmail.com>
2022-08-15 16:07:06 -0500

    Create IncrementDecrement.java

---
+++ IncrementDecrement.java
@@ -0,0 +1,21 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int lives = 5;
+        lives = lives - 1;
+        System.out.println(lives);
+
+        lives--; //Decremento
+        System.out.println(lives);
+
+        lives++; //Incremento
+        System.out.println(lives);
+
+        //Prefijo
+        //Gana un regalo por ganar una vida
+        int gift = 100 + lives++;
+        System.out.println(gift);
+
+
+    }
+}
